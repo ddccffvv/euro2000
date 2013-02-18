810d6ed45c6a0089925d8c932ddb22a09c400d41
OpenShift guest <stijnmuylle@gmail.com>
2013-02-18 11:04:40 -0500

    adding to re-intro

--- static/example.js
+++ static/example.js
@@ -105,7 +105,7 @@ $(document).ready(function() {
   $("#paid").blur(update_total);
    
   $("#addrow").click(function(){
-    $(".item-row:last").after('<tr class="item-row"><td class="item-name"><div class="delete-wpr"><textarea>Item Name</textarea><a class="delete" href="javascript:;" title="Remove row">X</a></div></td><td class="description"><textarea>Description</textarea></td><td><textarea class="cost">€10</textarea></td><td><textarea class="qty">1</textarea></td><td><span class="price"><textarea class="pricearea">€0</textarea></span></td></tr>');
+    $(".item-row:last").after('<tr class="item-row"><td class="item-name"><div class="delete-wpr"><textarea>Item Name</textarea><a class="delete" href="javascript:;" title="Remove row">X</a></div></td><td class="description"><textarea>Description</textarea></td><td><textarea class="cost">€10</textarea></td><td><textarea class="qty">1</textarea></td><td><span class="price">€0</span></td></tr>');
     update_price();
     if ($(".delete").length > 0) $(".delete").show();
     bind();
